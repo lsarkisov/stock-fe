c73f24cabc5b457303f076aefaf7c0d81f67acda
coder <coder@coders-MacBook-Pro.local>
2021-01-24 23:17:46 +0100

    removed unused file

--- src/const/index.js
+++
@@ -1,8 +0,0 @@
-import { REQUEST, SUCCESS, FAILURE } from './requests'
-
-function createRequestTypes(base) {
-  return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
-    acc[type] = `${base}_${type}`
-    return acc
-  }, {})
-}
